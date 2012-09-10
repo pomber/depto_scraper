e999e5c3393234badb387d1c310c7cfc23a2940e
Rodrigo Pombo <pombopombopombo@gmail.com>
2012-09-09 23:44:20 -0300

    missing files

---
+++ depto.py
@@ -0,0 +1,10 @@
+class Depto:
+	def __init__(self):
+		self.id = None
+		self.url = None
+		self.price = None
+		self.address = None
+		self.surface = None
+		self.photo_count = None
+		self.expenses = None
+		self.room_count = None
